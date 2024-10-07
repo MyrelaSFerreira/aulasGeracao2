f75761be1caef1ce8959759e6859f2e42e9cc0ce
MyrelaSFerreira <myrelaferreira01@gmail.com>
2024-10-07 14:31:27 -0300

    Add files via upload

---
+++ myrela/aulasGeracao2/aula 17/função.js
@@ -0,0 +1,15 @@
+function saudarUsuario(nome = "blublu"){
+    console.log(`Ola senhor ${nome}`)
+}
+
+
+saudarUsuario('blabla')
+
+// Criar uma fução  que retorne o dobro de um numero
+
+// let numero = Number(prompt())
+
+function dobro(numero = 30) {
+    console.log(`O dobro do numero ${numero} é igual a ${numero * 2}`)   
+}
+dobro(20);
